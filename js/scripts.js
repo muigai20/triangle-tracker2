53b1d62966362a36f2af3414189eab2ec14294c6
joseph-kamau <muigaijosephkamau@gmail.com>
2019-06-02 20:48:28 +0300

    added a function discover in js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function discover(){
+  var side1=parseint(document.getElementById('side1'));
+  var side2=parseint(document.getElementById('side2'));
+  var side3=parseint(document.getElementById('side3'));
+  var display=
+}
