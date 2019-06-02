93089d5b807fa0f6490e059a9f5b1a5e7c0e860d
joseph-kamau <muigaijosephkamau@gmail.com>
2019-06-02 20:51:23 +0300

    made my sides variables

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,5 @@ function discover(){
   var side1=parseint(document.getElementById('side1'));
   var side2=parseint(document.getElementById('side2'));
   var side3=parseint(document.getElementById('side3'));
-  var display=
+  var display= document.getElementById('display');
 }
