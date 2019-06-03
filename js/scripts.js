da049f1c53e994fd5a1d846d879cb9a14490ff27
joseph-kamau <muigaijosephkamau@gmail.com>
2019-06-03 09:38:44 +0300

    added if and if else conditions in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 function discover(){
-  var side1=parseint(document.getElementById('side1'));
-  var side2=parseint(document.getElementById('side2'));
-  var side3=parseint(document.getElementById('side3'));
-  var display= document.getElementById('display');
+var side1=parseInt(document.getElementById('side1').value);
+var side2=parseInt(document.getElementById('side2').value);
+var side3=parseInt(document.getElementById('side3').value);
+var display=document.getElementById('display');
+if(side1<=0&&side2<=0&&side3<=0){
+  display.textContent='This value do not make up a triangle'
+}else if(side1==side2&&side1==side3&&side2==side3){
+  display.textContent='This is an Equilateral Triangle : All sides are equal'
+}
+  }
+}function refresh(){
+  var side1=parseInt(document.getElementById('side1').value);
+  var side2=parseInt(document.getElementById('side2').value);
+  var side3=parseInt(document.getElementById('side3').value);
+  var display=document.getElementById('display');
+}
 }
